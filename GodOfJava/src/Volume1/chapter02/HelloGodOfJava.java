b1122b4d2848d811e5e69f74c7966504852ab98b
cabbage556 <liberty556@gmail.com>
2024-03-22 21:43:33 +0900

    자바의 신: 1장, 2장

---
+++ GodOfJava/src/Volume1/chapter02/HelloGodOfJava.java
@@ -0,0 +1,15 @@
+package Volume1.chapter02;
+
+public class HelloGodOfJava {
+    /**
+     * main() 메서드
+     *      java 명령어로 실행하는 자바 프로그램의 진입점
+     *      자바 프로그램에 있는 수많은 클래스 중 main() 메서드를 갖는 클래스가 적어도 하나 존재함
+     *
+     * @param args
+     */
+    public static void main(String args[]) {
+        System.out.println("Hello God of Java!!");
+        System.out.println("I love this book.");
+    }
+}
---
+++ GodOfJava/src/Volume1/chapter02/Profile.java
@@ -0,0 +1,8 @@
+package Volume1.chapter02;
+
+public class Profile {
+    public static void main(String[] args) {
+        System.out.println("My name is cabbage556");
+        System.out.println("My age is 32");
+    }
+}
