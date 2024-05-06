c5b02a98e73b9b23051a63b0c730b9834c66b546
cabbage556 <liberty556@gmail.com>
2024-05-06 20:57:03 +0900

    스프링 입문을 위한 자바 객체 지향의 원리와 이해: 7장(스프링을 통한 의존성 주입 - XML 파일에서 속성 주입)

---
+++ OOP-for-Spring-chapter7/src/main/java/com/cabbage556/OOPforSpringchapter7/expert003/Driver.java
@@ -0,0 +1,15 @@
+package com.cabbage556.OOPforSpringchapter7.expert003;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Driver {
+    public static void main(String[] args) {
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("expert003.xml");
+
+        // id="car" 속성을 갖는 bean을 가져옴
+        Car aCar = context.getBean("car", Car.class);
+        System.out.println(aCar.getTireBrand());
+    }
+}
---
+++ OOP-for-Spring-chapter7/src/main/java/com/cabbage556/OOPforSpringchapter7/expert003/Tire.java
@@ -0,0 +1,5 @@
+package com.cabbage556.OOPforSpringchapter7.expert003;
+
+public interface Tire {
+    String getBrand();
+}
