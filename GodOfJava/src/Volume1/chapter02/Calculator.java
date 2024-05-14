e9d45b1110feb732fca458fa55023e82e0026bf1
cabbage556 <liberty556@gmail.com>
2024-05-15 00:15:42 +0900

    자바의 신 복습: 2장

--- GodOfJava/src/Volume1/chapter02/Calculator.java
+++ GodOfJava/src/Volume1/chapter02/Calculator.java
@@ -2,11 +2,26 @@ package Volume1.chapter02;
 
 public class Calculator {
 
-    // public, static: 제어자(modifier)
-    // void: 리턴 타입
-    // main: 메서드 이름
-    // String[] args: 매개변수 목록
-    // System.out.~: 메서드 바디(중괄호 내부 내용들)
+    /*
+        메서드는 여섯 부분으로 나눌 수 있음
+            - 제어자(modifier) : 메서드의 특성을 정하는 부분
+                - public static
+            - 리턴 타입(return type) : 메서드 종료 시 돌려주는 타입
+                - void
+            - 메서드 이름 : 소괄호 앞에 있는 메서드 이름
+                - main
+            - 매개변수 목록 : 소괄호 안에 있는 매개 변수의 목록
+                - String[] args
+            - 예외 목록 : 소괄호와 중괄호 사이에 예외 목록을 선언할 수 있음
+                - 이 메서드에는 없음
+            - 메서드 내용 : 중괄호 내부 코드들
+                - System.out.println("Calculator.main");
+
+        메서드 선언 시 필수적인 것
+            - 리턴 타입
+            - 메서드 이름
+            - 메서드 내용
+     */
     public static void main(String[] args) {
         System.out.println("Calculator.main");
     }
--- GodOfJava/src/Volume1/chapter02/Profile.java
+++ GodOfJava/src/Volume1/chapter02/Profile.java
@@ -1,7 +1,9 @@
 package Volume1.chapter02;
 
+// 연습문제 : Profile 클래스 작성하기
 public class Profile {
 
+    // main() 메서드 선언 : public static void main(String[] args) {}
     public static void main(String[] args) {
         System.out.println("My name is cabbage556");
         System.out.println("My age is 32");
